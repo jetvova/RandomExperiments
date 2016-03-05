e9feddb141002ad8d8c4e6ed81da312412f5e5aa
Vova Ivanov <jetvova@gmail.com>
2016-03-04 16:22:14 -0800

    Made program remove outliers with a factor of 3 SIGMA

--- FileReaderAndAverager.py
+++ FileReaderAndAverager.py
@@ -1,3 +1,26 @@
+import numpy
+import numpy as np
+
+import numpy as np
+
+
+def reject_outliers(data):
+    u = np.mean(data)
+    s = np.std(data)
+    filtered = [e for e in data if (u - 3 * s < e < u + 3 * s)]
+    return filtered
+
+
+d = [2, 4, 5, 1, 6, 5, 40]
+filtered_d = reject_outliers(d)
+
+
+# print filtered_d
+
+# [2,4,5,1,6,5]
+
+
+
 def read(filename):
     with open(filename) as f:
         content = f.readlines()
@@ -6,13 +29,19 @@ def read(filename):
     return content
 
 
+a = read("a.txt")
+b = read("b.txt")
+
+
+
+
+
 def mean(listname):
     avg = sum(listname) / float(len(listname))
     return avg
 
-
-a = read("a.txt")
-b = read("b.txt")
+a = reject_outliers(a)
+b = reject_outliers(b)
 
 meana = mean(a)
 meanb = mean(b)
@@ -33,3 +62,24 @@ elif meana < meanb:
     print comparator(meanb, meana)
 else:
     print "The experiments yielded the same result"
+
+print """
+
+Standard deviation of experiment A:"""
+
+print numpy.std(a)
+
+print """
+
+Standard deviation of experiment B:"""
+
+print numpy.std(b)
+
+"""
+for i in range (0, len(a)):
+    if (a(i) - meana) > 3:
+        a.remove(i)
+"""
+
+
+
